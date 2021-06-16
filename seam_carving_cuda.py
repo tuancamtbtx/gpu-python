167d0a4069e67c5cc8ebee008d99173bfffdb9fb
tuhp-tech <tuhp.tech@gmail.com>
2021-06-16 20:25:33 +0700

    temporary commit

--- seam_carving_cuda.py
+++ seam_carving_cuda.py
@@ -8,8 +8,6 @@ from numba import jit, njit, cuda
 
 import argparse
 
-from seam_carving_cuda import seam_carving_kernel
-
 # ignore numba warning
 from numba.core.errors import NumbaDeprecationWarning, NumbaPendingDeprecationWarning, NumbaWarning
 import warnings
@@ -20,19 +18,13 @@ warnings.simplefilter('ignore', category=NumbaWarning)
 
 
 
+@cuda.jit
+def rgb2gray_kernel(rgb_img, gray_img):
+    i, j = cuda.grid(2)
 
-@njit
-def rgb2gray(img):
-    img = img.astype(np.float64)
-    h, w = img.shape[:2]
-    gray_img = np.zeros((h, w), dtype=np.float64)
-    for i in range(h):
-        for j in range(w):
-            gray_img[i][j] = img[i][j][0]*0.2989 + \
-                img[i][j][1]*0.5870 + img[i][j][2]*0.1140
-
-    return gray_img
-
+    if i < rgb_img.shape[0] and j < rgb_img.shape[1]:
+        gray_img[i,j] = 0.2989*rgb_img[i,j,0] + 0.5870*rgb_img[i,j,1] + 0.1140*rgb_img[i,j,2]
+    return
 
 def rotate_image(img, clockwise):
     k = 1 if clockwise else 3
@@ -40,41 +32,9 @@ def rotate_image(img, clockwise):
 
 
 @njit
-def calc_energy(img):
-    # convert rgb to grayscale
-    grayscale = rgb2gray(img)
-
-    filter_dx = np.array([
-        [1.0, 2.0, 1.0],
-        [0.0, 0.0, 0.0],
-        [-1.0, -2.0, -1.0],
-    ])
-    filter_dy = np.array([
-        [1.0, 0.0, -1.0],
-        [2.0, 0.0, -2.0],
-        [1.0, 0.0, -1.0],
-    ])
-
-    # Add zero padding to the input image
-    image_padded = np.zeros((grayscale.shape[0] + 2, grayscale.shape[1] + 2))
-    image_padded[1:-1, 1:-1] = grayscale
-
-    # energy_map = np.absolute(convolve(grayscale, filter_du)) + np.absolute(convolve(grayscale, filter_dv))
-    energy_map = np.zeros_like(grayscale)
-    for x in range(grayscale.shape[1]):
-        for y in range(grayscale.shape[0]):
-            # element-wise multiplication of the kernel and the image
-            gx = (filter_dx * image_padded[y: y+3, x: x+3]).sum()
-            gy = (filter_dy * image_padded[y: y+3, x: x+3]).sum()
-            energy_map[y, x] = np.absolute(gx) + np.absolute(gy)
-
-    return energy_map
-
-@njit
 def forward_energy(img):
-    height, width, _ = img.shape
-
-    img = rgb2gray(img)
+    height, width = img.shape[:2]
+    # img = rgb2gray(img)
 
     energy = np.zeros((height, width))  # energy table
     m = np.zeros((height, width))
@@ -144,7 +104,6 @@ def forward_energy(img):
 @jit
 def get_minimum_seam(img):
     h, w = img.shape[:2]
-
     # matrix to store minimum energy value seen upon pixel
     M = forward_energy(img)
     backtrack = np.zeros_like(M, dtype=np.uint16)
@@ -183,36 +142,69 @@ def get_minimum_seam(img):
     seam_idx = [seam_idx[-i - 1] for i in range(len(seam_idx))]
     return np.array(seam_idx), bool_mask
 
+@cuda.jit
+def find_seams_index_by_rows(bool_mask, bool_mask_index):
+    i, j = cuda.grid(2)
+    if i < bool_mask.shape[0] and j < bool_mask.shape[1]:
+        if bool_mask[i][j]:
+            bool_mask_index[i] = j
+            # bool_mask_index[i] = bool_mask.shape[1]
 
-@njit
-def remove_seam(img, bool_mask):
-    height, width, chanel = img.shape
+    return
 
-    output = np.empty((height, width-1, chanel), dtype=np.float64)
-    for row in range(height):
-        output_col = 0
-        for col in range(width):
-            if bool_mask[row][col]:
-                for ch in range(chanel):
-                    output[row][output_col][ch] = img[row][col][ch]
-                output_col += 1
+
+@cuda.jit
+def remove_seam_kernel(img, out_img, bool_mask_index):
+    i, j = cuda.grid(2)
+
+    if i < img.shape[0] and j < img.shape[1]:
+        for ch in range(img.shape[2]):
+            if j >= bool_mask_index[i][0]:
+                out_img[i][j][ch] = img[i][j + 1][ch]
             else:
-                continue
+                out_img[i][j][ch] = img[i][j][ch]
 
-    return output
+    return
 
 
-@jit
-def remove_seams(img, num_remove, rot=False):
+def remove_seams_kernel(img, num_remove, rot=False):
+    in_img = img.copy()
+
     for _ in range(num_remove):
-        _, bool_mask = get_minimum_seam(img)
-        img = remove_seam(img, bool_mask)
 
-    return img
+        ### convert rgb to grayscale
+        #Run kernel
+        griddim = 200, 230
+        blockdim = 32, 32
 
+        # Send to GPU
+        d_img = cuda.to_device(in_img)
+        d_gray_out = cuda.device_array(in_img.shape[0:2])
 
-@njit
-def insert_seam(img, seam_idx):
+        rgb2gray_kernel[griddim, blockdim](d_img, d_gray_out)
+
+        gray_img = np.asarray(d_gray_out)
+
+        ### get minimum seam
+        _, bool_mask = get_minimum_seam(gray_img)
+
+        ### remove seam
+        # Send to GPU
+        d_bool_mask = cuda.to_device(bool_mask)
+        d_bool_mask_index = cuda.device_array((bool_mask.shape[0], 1))
+        d_out = cuda.device_array((in_img.shape[0], in_img.shape[1] - 1, in_img.shape[2]))
+
+        find_seams_index_by_rows[griddim, blockdim](d_bool_mask, d_bool_mask_index)
+        # print(np.asarray(d_bool_mask_index).shape)
+        remove_seam_kernel[griddim, blockdim](d_img, d_out, d_bool_mask_index)
+
+        in_img = np.asarray(d_out).astype(np.uint8)
+
+    print(in_img.shape)
+    return in_img
+
+@cuda.jit
+def insert_seam_kernel(img, seam_idx):
     height, width, chanel = img.shape
     output = np.zeros((height, width+1, chanel))
     # The inserted pixel values are derived from an
@@ -234,7 +226,7 @@ def insert_seam(img, seam_idx):
     return output
 
 
-def insert_seams(img, num_insert):
+def insert_seams_kernel(img, num_insert):
     temp_img = img.copy()  # create replicating image from the input image
     seams_record = []
     for _ in range(num_insert):
@@ -246,7 +238,7 @@ def insert_seams(img, num_insert):
 
     for _ in range(num_insert):
         seam = seams_record.pop()
-        img = insert_seam(img, seam)
+        img = insert_seam_kernel(img, seam)
 
         # update remaining seam indices
         for remain_seam in seams_record:
@@ -254,8 +246,7 @@ def insert_seams(img, num_insert):
 
     return img
 
-
-def seam_carving(img, dx, dy):
+def seam_carving_kernel(img, dx, dy):
     img = img.astype(np.float64)
     h, w = img.shape[:2]
 
@@ -264,61 +255,19 @@ def seam_carving(img, dx, dy):
     output = img
 
     if dx < 0:
-        output = remove_seams(output, -dx)
+        output = remove_seams_kernel(output, -dx)
 
     elif dx > 0:
-        output = insert_seams(output, dx)
+        output = insert_seams_kernel(output, dx)
 
     if dy < 0:
         output = rotate_image(output, True)
-        output = remove_seams(output, -dy, rot=True)
+        output = remove_seams_kernel(output, -dy, rot=True)
         output = rotate_image(output, False)
 
     elif dy > 0:
         output = rotate_image(output, True)
-        output = insert_seams(output, dy, rot=True)
+        output = insert_seams_kernel(output, dy, rot=True)
         output = rotate_image(output, False)
 
     return output
-
-
-if __name__ == '__main__':
-    arg_parse = argparse.ArgumentParser()
-
-    arg_parse.add_argument(
-        "-mode", help="Type of running seam: cpu or gpu", type=str, default=0)
-
-    arg_parse.add_argument(
-        "-dy", help="Number of vertical seams to add/subtract", type=int, default=0)
-    arg_parse.add_argument(
-        "-dx", help="Number of horizontal seams to add/subtract", type=int, default=0)
-
-    arg_parse.add_argument("-in", help="Path to image", required=True)
-    arg_parse.add_argument("-out", help="Output file name", required=True)
-
-    args = vars(arg_parse.parse_args())
-
-    IN_IMG, OUT_IMG = args["in"], args["out"]
-
-    img = cv2.imread(IN_IMG)
-    assert img is not None
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-
-    print(img.shape)
-
-    if args["mode"] == "gpu":
-        # TODO: resize input images base on dx and dy seam number
-        dx, dy = args["dx"], args["dy"]
-        assert dx is not None and dy is not None
-        output = seam_carving_kernel(img, dx, dy)
-
-        output = cv2.cvtColor(output, cv2.COLOR_RGB2BGR)
-        cv2.imwrite(OUT_IMG, output)
-        pass
-
-    elif args["mode"] == "cpu":
-        # TODO: resize input images base on dx and dy seam number
-        dx, dy = args["dx"], args["dy"]
-        assert dx is not None and dy is not None
-        output = seam_carving(img, dx, dy)
-        cv2.imwrite(OUT_IMG, output)
